7288bf6eededcd7519516e0a932ce7467cc60cea
gatieme <gatieme@163.com>
2016-04-19 16:20:25 +0800

    定位bug002的故障位置, 原因未知...

--- src/engine/.memoryEngine.ko.cmd
+++
@@ -1,1 +0,0 @@
-cmd_/home/gatieme/Work/MemoryFaultInjection/src/engine/memoryEngine.ko := ld -m elf_x86_64 -r -o /home/gatieme/Work/MemoryFaultInjection/src/engine/memoryEngine.ko /home/gatieme/Work/MemoryFaultInjection/src/engine/memoryEngine.o /home/gatieme/Work/MemoryFaultInjection/src/engine/memoryEngine.mod.o
