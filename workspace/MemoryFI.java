7472b8ac880a716c8d13df7db33f58f0be3d6e13
gatieme <gatieme@163.com>
2016-05-26 11:27:33 +0800

    。。。

--- workspace/MemoryFI.java
+++ workspace/MemoryFI.java
@@ -186,3 +186,77 @@ public class MemoryFI
         System.out.println("RESULT : " + result);
     }
 }
+
+/*
+    返回码的格式为code(name)
+    其中code为整数, 0表示程序正常
+    nam标识了程序的运行状态
+
+    code = 0时, name可以是RUN, KT_RUN
+    code != 0 时, name为接收到到信号量,定义如下
+		case 1:
+			return "SIGHUP";
+		case 2:
+			return "SIGINT";
+		case 3:
+			return "SIGQUIT";
+		case 4:
+			return "SIGILL";
+		case 5:
+			return "SIGTRAP";
+		case 6:
+			return "SIGABRT";	//or "SIGIOT"
+		case 7:
+			return "SIGBUS";
+		case 8:
+			return "SIGFPE";
+		case 9:
+			return "SIGKILL";
+		case 10:
+			return "SIGUSR1";
+		case 11:
+			return "SIGSEGV";
+		case 12:
+			return "SIGUSR2";
+		case 13:
+			return "SIGPIPE";
+		case 14:
+			return "SIGALRM";
+		case 15:
+			return "SIGTERM";
+		case 16:
+			return "SIGSTKFLT";
+		case 17:
+			return "SIGCHLD";
+		case 18:
+			return "SIGCONT";
+		case 19:
+			return "SIGSTOP";
+		case 20:
+			return "SIGTSTP";
+		case 21:
+			return "SIGTTIN";
+
+		case 22:
+			return "SIGTTOU";
+		case 23:
+			return "SIGURG";
+		case 24:
+			return "SIGXCPU";
+		case 25:
+			return "SIGXFSZ";
+		case 26:
+			return "SIGVTALRM";
+		case 27:
+			return "SIGPROF";
+		case 28:
+			return "SIGWINCH";
+		case 29:
+			return "SIGIO"; 	// or "SIGPOLL"
+		case 30:
+			return "SIGPWR";
+		case 31:
+			return "SIGSYS";
+		default:
+			cerr << "unknown signal" << endl;
+			return "unknown";
